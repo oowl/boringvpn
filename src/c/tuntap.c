a31f7b90b4fa059e2a5463cef2a5f067245d3a9d
Attenuation <ouyangjun1999@gmail.com>
2019-05-23 20:20:41 +0800

    done c ffi

--- src/c/tuntap.c
+++ src/c/tuntap.c
@@ -1,18 +1,24 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <net/route.h>    
-#include <netinet/in.h>   
+#include <netinet/in.h>
+#include <arpa/inet.h>   
+#include <unistd.h>
 int32_t setup_dev(int32_t fd,char *ifname,short flags) {
     struct ifreq ifr;
     int err;
     memset(&ifr,0,sizeof(ifr));
     ifr.ifr_flags = flags;
     strncpy(ifr.ifr_name,ifname,IFNAMSIZ);
-    if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ){
-        pclose(fd);
+    if( (err = ioctl(fd, TUNSETIFF, &ifr)) < 0 ){
+        close(fd);
         return err;
     }
     strncpy(ifname,ifr.ifr_name,IFNAMSIZ);
@@ -27,9 +33,45 @@ int32_t setup_tun_device(int32_t fd, char *ifname) {
   return setup_dev(fd, ifname, IFF_TUN | IFF_NO_PI);
 }
 
-int32_t set_route(int sockfd, char *gateway_addr, char *dst_ip, char *mask) {
+int32_t up_device(char *name) {
+    struct ifreq ifr;
+    int sockfd;
+    
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+    printf("Create socket fails!\n");
+    return -1;	
+    }
+    
+    strncpy(ifr.ifr_name, name,IFNAMSIZ);
+    if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) < 0)
+    {
+    printf("ioctl SIOCGIFFLAGS fails!\n");
+    close(sockfd);
+    return -1;	
+    }
+    
+    ifr.ifr_flags |= IFF_UP;
+    if (ioctl(sockfd, SIOCSIFFLAGS, &ifr) < 0)
+    {
+    printf("ioctl SIOCSIFFLAGS fails!\n");
+    close(sockfd);
+    return -1;	
+    }
+    
+    close(sockfd);
+    
+    return 1;
+}
+
+int32_t set_route(char *dev,char *dst_ip, char *mask,char *gateway_addr) {
     struct rtentry route;
     struct sockaddr_in *addr;
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1){
+      perror("socket creation failed\n");
+      return -1;
+    }
     int err = 0;
     memset(&route, 0, sizeof(route));
     addr = (struct sockaddr_in*) &route.rt_gateway;
@@ -43,53 +85,63 @@ int32_t set_route(int sockfd, char *gateway_addr, char *dst_ip, char *mask) {
     addr->sin_addr.s_addr = inet_addr(mask);
     route.rt_flags = RTF_UP | RTF_GATEWAY;
     route.rt_metric = 100;
+    route.rt_dev = dev;
     err = ioctl(sockfd, SIOCADDRT, &route);
     if ((err) < 0) {
-    return -1;
+        return -1;
     }
     return 1;
 }
 
-int set_ip(char *iface_name, char *ip_addr, char *gateway_addr)
-{
-    if(!iface_name)
-    return -1;
+
+int32_t set_ip(char *name,char *ip_addr,char *netmask) {
+    up_device(name);
+    int sockfd;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+    printf("Create socket fails!\n");
+        return -1;	
+    }
     struct ifreq ifr;
     struct sockaddr_in sin;
-    int sockfd = create_socket();
-
     sin.sin_family = AF_INET;
-
-    // Convert IP from numbers and dots to binary notation
     inet_aton(ip_addr,&sin.sin_addr.s_addr);
-
-    /* get interface name */
-    strncpy(ifr.ifr_name, iface_name, IFNAMSIZ);
-
-    /* Read interface flags */
-    ioctl(sockfd,SIOCGIFFLAGS,&ifr);
-    /*
-    * Expected in <net/if.h> according to
-    * "UNIX Network Programming".
-    */
-    #ifdef ifr_flags
-    # define IRFFLAGS       ifr_flags
-    #else   /* Present on kFreeBSD */
-    # define IRFFLAGS       ifr_flagshigh
-    #endif
-    // If interface is down, bring it up
-    if (ifr.IRFFLAGS | ~(IFF_UP)) {
-        ifr.IRFFLAGS |= IFF_UP;
-        ioctl(sockfd,SIOCSIFFLAGS,&ifr);
-    }
-    // Set route
-    // set_route(sockfd, gateway_addr);
     memcpy(&ifr.ifr_addr, &sin, sizeof(struct sockaddr)); 
-    // Set interface address
     if (ioctl(sockfd, SIOCSIFADDR, &ifr) < 0) {
         return -1;
-    }             
-    #undef IRFFLAGS 
-
-    return 0;
+    }
+    inet_aton(netmask,&sin.sin_addr.s_addr);
+    memcpy(&ifr.ifr_netmask, &sin, sizeof(struct sockaddr)); 
+    if (ioctl(sockfd, SIOCSIFNETMASK, &ifr) < 0) {
+        return -1;
+    }
 }
+
+// int main() {
+//     int fd, err;
+
+//     if( (fd = open("/dev/net/tun", O_RDWR)) < 0 )
+//         return -1;
+//     char dev[IFNAMSIZ];
+//     memset(&dev,0,sizeof(dev));
+//     strncpy(dev,"tun1",4);
+//     if (setup_tun_device(fd,dev) < 0){
+//         printf("setup failed\n");
+//         return -1;
+//     }
+//     if (up_device(dev) < 0){
+//         printf("up failed\n");
+//         return -1;
+//     }
+//     if (set_ip(dev,"192.168.1.2","255.255.255.0") < 0){
+//         printf("set_ip failed\n");
+//         return -1;
+//     }
+//     if (set_route(dev,"192.168.2.0","255.255.255.0","192.168.1.1") < 0) {
+//         printf("set route failed\n");
+//         return -1;
+//     }
+//     sleep(10);
+//     close(fd);
+//     return 0;
+// }
